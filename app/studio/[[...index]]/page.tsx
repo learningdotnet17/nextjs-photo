aaadc2e420cb85da736bc79252796e37944a7edc
learningdotnet17 <bcatdc@hotmail.com>
2025-10-07 12:36:16 -0400

    fix: add client-side hydration check for Sanity Studio

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -2,8 +2,29 @@
 
 import { NextStudio } from "next-sanity/studio"
 import config from "@/sanity/config"
+import { useEffect, useState } from "react"
 
 export default function StudioPage() {
+  const [isClient, setIsClient] = useState(false)
+  
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return (
+      <div style={{ 
+        display: 'flex', 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        height: '100vh',
+        fontFamily: 'system-ui, sans-serif'
+      }}>
+        <div>Loading Sanity Studio...</div>
+      </div>
+    )
+  }
+
   return <NextStudio config={config} />
 }
 
